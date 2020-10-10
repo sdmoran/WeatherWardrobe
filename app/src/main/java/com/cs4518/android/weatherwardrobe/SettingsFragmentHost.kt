b1de905d0d93b0d8582d60aaad5a8a73511bb69c
karoad <krhodes@qr8health.com>
2020-10-10 17:05:53 -0400

    Added light/dark mode functionality

--- app/src/main/java/com/cs4518/android/weatherwardrobe/SettingsFragmentHost.kt
+++ app/src/main/java/com/cs4518/android/weatherwardrobe/SettingsFragmentHost.kt
@@ -1,12 +1,19 @@
 package com.cs4518.android.weatherwardrobe
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.ListPreference
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
 
-class SettingsFragmentHost : AppCompatActivity() {
+class SettingsFragmentHost : AppCompatActivity(), SharedPreferences.OnSharedPreferenceChangeListener, Preference.SummaryProvider<ListPreference>{
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val darkModeValues = resources.getStringArray(R.array.dark_mode_values)
         setContentView(R.layout.clothing_item_host)
 
         val currentFragment =
@@ -18,6 +25,38 @@ class SettingsFragmentHost : AppCompatActivity() {
                 .beginTransaction()
                 .add(R.id.fragment_container, fragment)
                 .commit()
+
+                supportActionBar?.setDisplayHomeAsUpEnabled(true)
+                PreferenceManager.getDefaultSharedPreferences(this)
+                    .registerOnSharedPreferenceChangeListener(this)
+
         }
     }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+        val darkModeString = getString(R.string.dark_mode)
+        key?.let {
+            if (it == darkModeString) sharedPreferences?.let { pref ->
+                val darkModeValues = resources.getStringArray(R.array.dark_mode_values)
+                when (pref.getString(darkModeString, darkModeValues[0])) {
+                    darkModeValues[0] -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+                    darkModeValues[1] -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                    darkModeValues[2] -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                    darkModeValues[3] -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY)
+                }
+            }
+        }
+    }
+
+    class AppSettingsFragment : PreferenceFragmentCompat() {
+
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey);
+
+        }
+    }
+
+    override fun provideSummary(preference: ListPreference?): CharSequence =
+        if (preference?.key == getString(R.string.dark_mode)) preference.entry
+        else "Unknown Preference"
 }
